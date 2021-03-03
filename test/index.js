ada91734816eaf2e2c705e71a7f5038517ad97ba
Thiago Lagden <lagden@gmail.com>
2021-03-03 18:46:08 -0300

    test using got

--- test/index.js
+++ test/index.js
@@ -1,32 +1,42 @@
 'use strict'
 
 const test = require('ava')
-const app = require('./helper/server')
+const got = require('got')
+const server = require('./helper/server')
+
+test.before(async t => {
+	t.context.baseUrl = await server()
+})
 
 test('hello', async t => {
-	const r = await app
-		.get('/')
+	const response = await got.get(`${t.context.baseUrl}/`, {
+		throwHttpErrors: false,
+		responseType: 'json'
+	})
 
-	const {hello} = r.body.data
-	t.is(r.status, 200)
+	const {hello} = response.body.data
+	t.is(response.statusCode, 200)
 	t.is(hello, 'Hello World')
 })
 
 test('hello boilerplate', async t => {
-	const r = await app
-		.get('/boilerplate')
+	const r = await got.get(`${t.context.baseUrl}/boilerplate`, {
+		throwHttpErrors: false,
+		responseType: 'json'
+	})
 
 	const {hello} = r.body.data
-	t.is(r.status, 200)
+	t.is(r.statusCode, 200)
 	t.is(hello, 'Hello boilerplate')
 })
 
 test('echo', async t => {
-	const r = await app
-		.post('/echo')
-		.set('content-type', 'application/json')
-		.send({xxx: true})
+	const r = await got.post(`${t.context.baseUrl}/echo`, {
+		throwHttpErrors: false,
+		responseType: 'json',
+		json: {xxx: true}
+	})
 
-	t.is(r.status, 200)
+	t.is(r.statusCode, 200)
 	t.true(r.body.xxx)
 })
